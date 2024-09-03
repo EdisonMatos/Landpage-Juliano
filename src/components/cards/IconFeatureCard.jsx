b220706cfa5fde78f480676f4cacf82e92ce63d0
Gabriel <gabrielsouza65115@gmail.com>
2024-09-03 09:21:09 -0300

    fix: Corrige tamanho dos cards da Features

--- src/components/cards/IconFeatureCard.jsx
+++ src/components/cards/IconFeatureCard.jsx
@@ -3,7 +3,7 @@ export default function IconFeatureCard(props) {
 
   return (
     <div
-      className={` tablet1:h-[300px] desktop1:h-[320px] desktop2:h-[320px] desktop3:h-[300px] desktop1:w-full des flex flex-col items-center desktop1:hover:scale-110 transition p-[18px] desktop1:p-0 desktop2:p-[18px] ${className}`}
+      className={` tablet1:h-[350px] desktop1:h-[320px] desktop2:h-[320px] desktop3:h-[300px] desktop1:w-full des flex flex-col items-center desktop1:hover:scale-110 transition p-[18px] desktop1:p-0 desktop2:p-[18px] ${className}`}
     >
       <div className="h-[64px] w-[64px] mb-[24px] bg-primary rounded-md flex justify-center items-center text-secondary">
         {icon}
